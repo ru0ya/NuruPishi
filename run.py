d3184122fa17bde7412bd0de63e0171c74a6015e
ru0ya <viktaangi02@gmail.com>
2023-04-10 21:40:05 +0300

    debug

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from nurupishi import create_app
 
+app = create_app(app_id="APP_ID", app_key="APP_KEY")
 
 if __name__ == "__main__":
-    app = create_app(app_id="APP_ID", app_key="APP_KEY")
-    app.run(debug=True)
+    app.run()
