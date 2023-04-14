6d1ddd1a294ea4103f2bdc73b4cfdf97255f562a
ru0ya <viktaangi02@gmail.com>
2023-04-14 06:54:49 +0000

    debug

--- nurupishi/app_plugins.py
+++ nurupishi/app_plugins.py
@@ -5,19 +5,11 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 
 login_manager = LoginManager()
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 migrate = Migrate()
-
-#login_manager.session_protection = "strong"
-#login_manager.login_view = "login"
-#login_manager.login_message_category = "info"
-
-#def init_app(app):
- #   login_manager.init_app(app)
-  #  db.init_app(app)
-   # bcrypt.init_app(app)
-
+mail = Mail()
